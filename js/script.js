b6a4e9938fb79b9aebc83a6b7ba6ee00b3ededfe
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-21 16:29:14 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+
+/**
+ * This function is calculates the area of a trapezoid
+*/
+'use strict'
+
+
+function calculate () {
+// input
+const length= parseFloat(document.getElementById('length').value)
+const width= parseFloat(document.getElementById('width').value)
+const height= parseFloat(document.getElementById('height').value)
+
+// process
+
+const volume= ((length * width * height) / 3) 
+
+// output
+document.getElementById('volume').innerHTML = 'volume is: ' + volume + ' mm³'
+}
